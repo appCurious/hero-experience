c0770f4af9f595fbdb077a3ae015f5b5b50d8cb3
gregclark <greg.clark@syndigo.com>
2021-05-06 09:44:59 -0500

    working through build process

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,18 @@
 // import urlResolve from 'rollup-plugin-url-resolve'
 // import {terser} from 'rollup-plugin-terser';
 import resolve  from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
 
 export default {
     plugins: [
         // urlResolve()
         // terser()
-        resolve()
+        resolve(),
+        commonjs()
     ],
     input: 'main.js',
     output: {
-        file: './public/myhero.js',
+        file: './public/index.js',
         name: 'MyHeroElement',
         format: 'cjs'
         // format: 'es'
